99c0615720b00c2814d00015728bb02c29007180
Bikash <bikashjha490@gmail.com>
2025-12-19 18:24:54 +0545

    initial commit

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+// https://pyth.network/developers/price-feed-ids#solana-stable
+pub const SOL_USD_FEED_ID: &str = "";
+pub const USDC_USD_FEED_ID: &str = "";
+pub const MAXIMUM_AGE: u64 = 100; 
+
